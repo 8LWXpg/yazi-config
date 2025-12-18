fa0b1ca1cd16f13c4b168c90fbcbbf9894c35a20
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-12-18 21:34:14 +0800

    add another folder

--- plugins/folder-rules.yazi/main.lua
+++ plugins/folder-rules.yazi/main.lua
@@ -1,7 +1,7 @@
 local function setup()
 	ps.sub('cd', function()
 		local cwd = cx.active.current.cwd
-		if cwd:ends_with('Downloads') or cwd:ends_with('Screenshots') then
+		if cwd:ends_with('Downloads') or cwd:ends_with('Screenshots') or cwd:ends_with('Captures') then
 			ya.emit('sort', { 'mtime', reverse = true, dir_first = false })
 		else
 			ya.emit('sort', { 'alphabetical', reverse = false, dir_first = true })
