2317268005aaa9a1777c45bc0edd179b9f613922
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2026-03-12 16:31:23 +0800

    add keymap for scp hovered file to ssh client, formatting

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-require('full-border'):setup()
-require('custom-shell'):setup({
+require("full-border"):setup()
+require("custom-shell"):setup({
 	save_history = false,
 })
-require('folder-rules'):setup()
-require('starship'):setup()
+require("folder-rules"):setup()
+require("starship"):setup()
